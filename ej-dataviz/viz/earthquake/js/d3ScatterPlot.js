ef3c68e464937a31a592aa5d5d16a5dd38dfe350
ynunokawa <yusuke_nunokawa@esrij.com>
2016-03-08 17:58:04 +0900

    add viz of earthquake

---
+++ ej-dataviz/viz/earthquake/js/d3ScatterPlot.js
@@ -0,0 +1,176 @@
+define([
+    // AppConfig
+    "config/defaults",
+    
+    // Time Animation
+    "esri/TimeExtent",
+
+    // Dojo Utils
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+    "dojo/dom-style", 
+    "dojo/dom-attr", 
+    "dojo/dom", 
+    "dojo/on", 
+    "dojo/query",
+    "dojo/_base/array",
+
+    "dojo/domReady!"
+], function (
+    appConfig,
+    
+    TimeExtent,
+
+    declare,
+    lang,
+	domStyle, 
+	domAttr, 
+	dom, 
+	on,
+	query,
+	arrayUtils
+  ){
+      return declare([], {
+		constructor: function() {
+			var options = arguments[0];
+			this.map = options.map;
+			this.data = options.data;
+            this.layer = options.layer;
+            this.domId = arguments[1];
+            this.svg = {};
+			this._initChart();
+		},
+
+		_initChart: function() {
+            console.log(this.data);
+            console.log(this.domId);
+            var data = this.data;
+            var layer = this.layer;
+            
+            var margin = {top: 20, right: 40, bottom: 30, left: 50},
+                width = window.innerWidth - margin.left - margin.right,
+                height = 300 - margin.top - margin.bottom;
+
+            var x = d3.time.scale()
+                .range([0, width]);
+            /*var x = d3.scale.linear()
+                .range([1451613639000, 1457413398000]);*/
+
+            var y = d3.scale.linear()
+                .range([height, 0]);
+
+            var color = d3.scale.category10();
+
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient("bottom");
+
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .orient("left");
+
+            var svg = d3.select("#" + this.domId).append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            
+            arrayUtils.forEach(data, function(d) {
+                d.MAGNITUDE = +d.MAGNITUDE;
+                d.UTC_DATETIME = +d.UTC_DATETIME;
+            });
+
+            x.domain(d3.extent(data, function(d) { return d.UTC_DATETIME; })).nice();
+            y.domain(d3.extent(data, function(d) { return d.MAGNITUDE; })).nice();
+
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .style("fill", "#9d9d9d")
+                .call(xAxis)
+                .append("text")
+                .attr("class", "label")
+                .attr("x", width)
+                .attr("y", -6)
+                .style("text-anchor", "end")
+                .style("fill", "#9d9d9d")
+                .text("Date");
+
+            svg.append("g")
+                .attr("class", "y axis")
+                .style("fill", "#9d9d9d")
+                .call(yAxis)
+                .append("text")
+                .attr("class", "label")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .style("fill", "#9d9d9d")
+                .text("Magnitude");
+
+            svg.selectAll(".dot")
+                .data(data)
+                .enter().append("circle")
+                .attr("class", "dot")
+                .attr("r", function(d) { return d.MAGNITUDE; })
+                .attr("cx", function(d) { return x(d.UTC_DATETIME); })
+                .attr("cy", function(d) { return y(d.MAGNITUDE); })
+                //.style("fill", function(d) { return color(d.MAGNITUDE); });
+                //.style("fill-opacity", 0)
+                //.style("stroke", "rgb(255, 85, 0)");
+                //.style("fill", "#9d9d9d")
+                .on("mouseover", lang.hitch(function(e) {
+                    arrayUtils.forEach(layer.graphics, function(g) {
+                        if(e.OBJECTID === g.attributes.OBJECTID) {
+                            console.log(g);
+                            console.log(g.getNode());
+                            d3.select(g.getNode()).attr("class", "active-feature");
+                        }
+                    });
+                }))
+                .on("mouseout", function() {
+                    d3.selectAll(".active-feature").attr("class", "");
+                });
+
+            /*var legend = svg.selectAll(".legend")
+                .data(color.domain())
+                .enter().append("g")
+                .attr("class", "legend")
+                .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+            legend.append("rect")
+                .attr("x", width - 18)
+                .attr("width", 18)
+                .attr("height", 18)
+                .style("fill", color);
+
+            legend.append("text")
+                .attr("x", width - 24)
+                .attr("y", 9)
+                .attr("dy", ".35em")
+                .style("text-anchor", "end")
+                .text(function(d) { return d; });*/
+                
+            this.svg = svg;
+		},
+        
+        setHighlightSpan: function(start, end) {
+            this.svg.selectAll(".active-dot")
+                .attr("class", "dot");
+            
+            this.svg.selectAll(".dot")
+                .attr("class", function(d) {
+                    //console.log(new Date(d.UTC_DATETIME));
+                    var date = new Date(d.UTC_DATETIME);
+                    //console.log(d);
+                    if(start <= date && end > date) {
+                        return "active-dot";
+                    }
+                    else {
+                        return "dot";
+                    }
+                });
+        }
+	});
+});
---
+++ ej-dataviz/viz/earthquake/js/main.js
@@ -0,0 +1,163 @@
+require([
+    // AppConfig
+    "config/defaults",
+
+    // Map
+    "esri/arcgis/utils",
+    
+    // Time Animation
+    "esri/TimeExtent",
+    
+    // Chart
+    "app/d3ScatterPlot",
+    "esri/tasks/query",
+    "esri/tasks/QueryTask",
+
+    // Dojo Utils
+    "dojo/dom-style", 
+    "dojo/dom-attr", 
+    "dojo/dom", 
+    "dojo/on", 
+    "dojo/query",
+    "dojo/_base/array",
+
+    "dojo/domReady!"
+], function (
+    appConfig,
+    
+	arcgisUtils,
+    
+    TimeExtent,
+    
+    Chart,
+    Query,
+    QueryTask,
+
+	domStyle, 
+	domAttr, 
+	dom, 
+	on,
+	dojoQuery,
+	arrayUtils
+  ){
+
+    var map, eq_depth, eq_magnitude, playInterval = 6200;
+    var now = { start: new Date("1/1/2016 UTC"), end: new Date("1/2/2016 UTC") };
+    var chart, eq_allData = [], nextStartId = 0;
+    var eq_queryTask = new QueryTask(appConfig.chart.dataUrl);
+
+    function initMap() {
+        var deferred = arcgisUtils.createMap(appConfig.map.id, "mapView", {
+            mapOptions: {
+                sliderPosition: "bottom-right"
+            }
+        }).then(function(response) {
+            map = response.map;
+            map.disableMapNavigation(); // 地図操作無効化
+            //console.log(map);
+            
+            //initTime();
+            getEQAllData();
+
+            eq_depth = map.getLayer(appConfig.layer.eq_depth);
+            eq_magnitude = map.getLayer(appConfig.layer.eq_magnitude);
+
+            eq_magnitude.on("graphic-draw", eq_magnitude_draw);
+            eq_magnitude.on("mouse-over", eq_magnitude_hover);
+            eq_magnitude.on("mouse-out", eq_magnitude_mouseout);
+        });
+    }
+    
+    function initChart(data) {
+        chart = new Chart({
+            map: map,
+            layer: eq_magnitude,
+            data: data
+        }, "chartView");
+        setTimeout(function() {
+            initTime();
+        }, 6200);
+    }
+    
+    function updateChart() {
+        chart.setHighlightSpan(now.start, now.end);
+    }
+    
+    function getEQAllData(startId) {
+        var query = new Query();
+        query.outFields = ["OBJECTID", "MAGNITUDE", "DEPTH", "UTC_DATETIME"];
+        query.returnGeometry = false;
+        //query.where = "1=1";
+        if(startId !== undefined) {
+            query.where = "MAGNITUDE >= 4 AND OBJECTID > " + startId;
+        }
+        else {
+            query.where = "MAGNITUDE >= 4";
+        }
+        
+        eq_queryTask.execute(query, successGetEQAllData, errorGetEQAllData);
+    }
+    
+    function successGetEQAllData(e) {
+        console.log("successGetEQAllData", e);
+        arrayUtils.forEach(e.features, function(f) {
+            if(f.attributes["UTC_DATETIME"] != null || f.attributes["UTC_DATETIME"] != undefined) {
+                eq_allData.push(f.attributes);
+            }
+        });
+        
+        if(e.exceededTransferLimit === true) {
+            nextStartId += 1000;
+            getEQAllData(nextStartId);
+        }
+        else {
+            console.log("finish: successGetEQAllData", eq_allData);
+            initChart(eq_allData);
+        }
+    }
+    
+    function errorGetEQAllData() {
+        console.log("errorGetEQAllData");
+    }
+    
+    function initTime() {
+        dom.byId("now").innerHTML = "<b>" + now.start.getFullYear() + "年 " + (now.start.getMonth() + 1) + "月 " + now.start.getDate() + "日</b>";
+        var timeExtent = new TimeExtent();
+        timeExtent.startTime = now.start;
+        timeExtent.endTime = now.end;
+        map.setTimeExtent(timeExtent);
+        console.log(timeExtent);
+        
+        updateChart();
+        
+        setTimeout(function() {
+            forwardTime();
+        }, playInterval);
+    }
+    
+    function forwardTime() {
+        now.start.setDate(now.start.getDate() + 1);
+        now.end.setDate(now.end.getDate() + 1);
+        initTime();
+    }
+
+    function eq_magnitude_draw(e) {
+        //console.log(e.node);
+        //console.log(e.graphic._shape.shape.r);
+        d3.select(e.node).attr("r", 0);
+        d3.select(e.node).transition()
+            .delay(0)
+            .duration(4000)
+            .attr("r", e.graphic._shape.shape.r);
+    }
+    
+    function eq_magnitude_hover(e) {
+        
+    }
+    
+    function eq_magnitude_mouseout(e) {
+        
+    }
+
+    initMap();
+});
